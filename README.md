24d3701e65a990101ea2b437f52ccdd67d4ee7c7
Jeremy Kennard <13806019+iamvalion@users.noreply.github.com>
2023-10-19 00:09:56 -0600

    Remove existing static files, update gitignore

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Valion's Web Hub
 
-This repository contains the code of my web home/portal, located at https://hub.valion.me. It is proudly powered by [Svelte](https://svelte.dev).
+This repository contains the code of my web home/portal, located at https://hub.valion.me. It is proudly powered by [Svelte](https://svelte.dev)([Kit](https://kit.svelte.dev)).
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+    <header class="site-header">
+        <img class="logo" src="images/Valion-Logo_Dark-Mountains_128x.png" alt="V logo on pink mountain background">
+    </header>
+
+    <main class="site-content">
+        <header class="page-header">
+            <h1 class="page-title">Hey, I'm Jeremy.</h1>
+            <span class="page-subtitle">Undergraduate computer science student, software and security enthusiast, audiovisual fanatic, creative thinker, challenger; pseudonymously known as Valion</span>
+        </header>
+
+        <section class="page-content">
+            <h2>About</h2>
+            <p>Welcome to my website. This page is more or less meant to be a placeholder while I focus my efforts on school and other projects. A more complete, overhauled version is in the works, though.</p>
+            <p>In the future, I plan to use this site to...</p>
+            <ul>
+                <li>demonstrate personal software and security projects;</li>
+                <li>showcase visual art, including design, illustration, and 3D modeling;</li>
+                <li>publish some music, hopefully in a variety of genres;</li>
+                <li>post about anything that interests me.</li>
+            </ul>
+            <h2>Work</h2>
+            <p>In the meantime, feel free to take a peek at some of my projects:</p>
+            <ul>
+                <li><a href="https://avilon.org" target="_blank">Avilon</a> - my eventual creative tech studio</li>
+                <li><a href="https://neu.systems" target="_blank">Neu</a> - systems for a new era of computing</li>
+                <li><a href="https://demo.valion.me/stratum-theme" target="_blank">Stratum</a> - a simple flat-file blog theme</li>
+            </ul>
+            <p>You can view more work on my <a href="https://github.com/iamvalion" target="_blank">GitHub</a>.</p>
+        </section>
+
+        <footer class="page-footer">
+            <nav class="social-links">
+                <a class="github-link" href="https://github.com/iamvalion" target="_blank"><i class="fa-brands fa-github"></i></a> |
+                <a class="dribbble-link" href="https://dribbble.com/iamvalion" target="_blank"><i class="fa-brands fa-dribbble"></i></a> |
+                <a class="linkedin-link" href="https://linkedin.com/in/iamvalion" target="_blank"><i class="fa-brands fa-linkedin"></i></a> |
+                <a class="twitter-link" href="https://twitter.com/iamvalion" target="_blank"><i class="fa-brands fa-twitter"></i></a> |
+                <a class="instagram-link" href="https://instagram.com/iamvalion" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            </nav>
+        </footer>
+    </main>
+
+    <footer class="site-footer">
+        <p class="copyright-text">&copy; <span id="copyright-year"></span> Jeremy Kennard. All rights reserved.</p>
+    </footer>
+</div>
